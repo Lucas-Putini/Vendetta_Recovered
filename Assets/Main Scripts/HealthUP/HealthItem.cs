37bddbab116b69cf2bbd0d112011011bf47e0c9b
Abdullah-at <abdullah.barzinji@gmail.com>
2025-04-15 23:39:59 +0200

    Revert "Health up"

--- Assets/Main Scripts/HealthUP/HealthItem.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "HealthItem", menuName = "Items/Health Item")]
-public class HealthItem : ScriptableObject
-{
-    public string itemName;
-    public Sprite icon;
-    [Range(0f, 1f)] public float healAmount;
-}
